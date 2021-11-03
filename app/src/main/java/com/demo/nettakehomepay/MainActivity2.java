3ed3ba5fc866c61025387fe49c8e5d01a29af847
Lanchuii <lanzmontelibano21@gmail.com>
2021-11-03 20:30:47 +0800

    net pay function

--- app/src/main/java/com/demo/nettakehomepay/MainActivity2.java
+++ app/src/main/java/com/demo/nettakehomepay/MainActivity2.java
@@ -1,6 +1,8 @@
 package com.demo.nettakehomepay;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -10,7 +12,6 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity2 extends AppCompatActivity {
 
     Button compute;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +19,7 @@ public class MainActivity2 extends AppCompatActivity {
 
         TextView displayname = (TextView) findViewById(R.id.nametvtxt);
         TextView displaysalary = (TextView) findViewById(R.id.salarytvtxt);
+        TextView displaytotal = (TextView) findViewById(R.id.finalSalary);
         EditText displaydeduction = (EditText) findViewById(R.id.deductionedtxt);
 
         compute = (Button) findViewById(R.id.btncompute);
@@ -25,7 +27,16 @@ public class MainActivity2 extends AppCompatActivity {
         displayname.setText(getIntent().getStringExtra("Name"));
         displaysalary.setText(getIntent().getStringExtra("Salary"));
 
+        compute.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                float currenttotal = Float.parseFloat(displaysalary.getText().toString());
+                float deduction = Float.parseFloat(displaydeduction.getText().toString());
+                float finaltotal = currenttotal-deduction;
+                displaytotal.setText("Total: " +finaltotal);
 
+            }
+        });
 
 
 
